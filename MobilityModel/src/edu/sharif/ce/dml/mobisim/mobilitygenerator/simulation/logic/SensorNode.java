ab6f393ee1cc03558fce36b944ac887115b6c824
jeberst <j_eberst@yahoo.com>
2013-11-04 21:36:03 -0500

    The Mobility Project for COP5711
    This is the first commit for the mobility project COP 5711

---
+++ MobilityModel/src/edu/sharif/ce/dml/mobisim/mobilitygenerator/simulation/logic/SensorNode.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.sharif.ce.dml.mobisim.mobilitygenerator.simulation.logic;
+
+/**
+ *
+ * @author Josh
+ */
+
+import edu.sharif.ce.dml.common.logic.entity.Location;
+
+import java.util.List;
+
+public class SensorNode extends GeneratorNode {
+    private SensorNode topnode;
+    private SensorNode bottomnode;
+    private SensorNode leftnode;
+    private SensorNode rightnode;
+    private boolean center;
+    GeneratorNode defaultnode;
+    
+    public SensorNode(GeneratorNode defaultnode, boolean center, SensorNode top,  SensorNode right, SensorNode bottom,  SensorNode left)
+    {
+        this.defaultnode = defaultnode;
+        this.center = center;
+        this.bottomnode = bottom;
+        this.topnode = top;
+        this.rightnode = right;
+        this.leftnode = left;
+    }
+    public SensorNode(GeneratorNode defaultnode, boolean center)
+    {
+        this.defaultnode = defaultnode;
+        this.center = center;
+    }
+    
+
+    public void setSensorLocation(Location location, SensorNode node)
+    {
+        if(!center)
+        {
+            if(leftnode == null && rightnode == null)
+            {
+                if(isCenter(bottomnode))
+                {
+                    //this is the topnode
+                }
+                else
+                {
+                    //this is the bottomnode
+                }
+            }
+            else if(topnode == null && bottomnode == null)
+            {
+                if(isCenter(leftnode))
+                {
+                    //this is the rightnode
+                }
+                else
+                {
+                    //this is the leftnode
+                }
+            }  
+        }
+        else
+        {
+            //this is the center
+        }
+    }
+    
+    public boolean isCenter(SensorNode node)
+    {
+        return node.center;
+    }
+    
+    //Public setCenter
+    //Public Set Node(s)
+    
+}
